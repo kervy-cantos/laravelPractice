0abad4c87edba49c6be790999bbcf08b15abe5a4
asoikaw15 <krteammain@gmail.com>
2022-09-01 15:08:25 +0800

    I created something only god knows what it is

--- database/migrations/2022_09_01_070631_create_workers_table.php
+++ database/migrations/2022_09_01_070631_create_workers_table.php
@@ -13,13 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('workers', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('taskName');
-            $table->float('laborTotal')->default('0');
-            $table->float('partsTotal')->default('0');
-            $table->float('taskTotal')->default('0');
-        });   
+            $table->foreignId('task_id')->constrained('tasks');
+            $table->string('workerName');
+            $table->float('workHours')->default('0');
+            $table->float('workRate')->default('0');
+            $table->float('workersTotal')->default('0');
+        });
+     
     }
 
     /**
@@ -29,7 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-  
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('workers');
     }
 };
